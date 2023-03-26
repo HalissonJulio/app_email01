bf027c4e1bc560540995ef548901c4025d4082d8
HalissonJulio <halissonjulio@gmail.com>
2023-03-25 23:14:18 -0300

    Baixado no pc de casa.

--- app/src/main/java/pereira/lopes/julio/app_email01/MainActivity.java
+++ app/src/main/java/pereira/lopes/julio/app_email01/MainActivity.java
@@ -2,13 +2,52 @@ package pereira.lopes.julio.app_email01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+        // Define a ação do click do botão
+        btnEnviar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Obtendo dados digitados pelo usuario
+                EditText etEmail = findViewById(R.id.etEmail);
+                EditText etAssunto = findViewById(R.id.etAssunto);
+                EditText etTexto = findViewById(R.id.etTexto);
+
+                // Guardando os dados em variaveis
+                String email = etEmail.getText().toString();
+                String assunto = etAssunto.getText().toString();
+                String texto = etTexto.getText().toString();
+
+                // Criando uma intent, como não exite mais uma tela ele irá para o link indicado no setData que é o do email
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+                i.setData(Uri.parse("mailto:"));
+
+                //Mandando os valores das variaveis para os campos do email, como: email, assunto e o texto
+                String[] emails = new String[]{email};
+                i.putExtra(Intent.EXTRA_EMAIL, emails);
+                i.putExtra(Intent.EXTRA_SUBJECT, assunto);
+                i.putExtra(Intent.EXTRA_TEXT, texto);
+
+                // Caso o usuário tenha mais apps que aceitam o compartilhamento, caso tenha você escolhe o app, caso não, o android te mostra a mensagem de erro
+                try {
+                    startActivity(Intent.createChooser(i, "Escolha o APP"));
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(MainActivity.this, "Não há nenhum app que posso realizar essa operação", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
