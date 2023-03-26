45105ce0726a5a5ef08a750aee68d2da31e23ff3
HalissonJulio <halissonjulio@gmail.com>
2023-03-26 00:31:03 -0300

    Projeto finalizado.

--- app/src/main/java/pereira/lopes/julio/app_email01/MainActivity.java
+++ app/src/main/java/pereira/lopes/julio/app_email01/MainActivity.java
@@ -17,31 +17,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btnEnviar = findViewById(R.id.btnEnviar);
-        // Define a ação do click do botão
+        // Define a ação do click do botão.
         btnEnviar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                // Obtendo dados digitados pelo usuario
+                // Obtem os dados digitados pelo usúario.
                 EditText etEmail = findViewById(R.id.etEmail);
                 EditText etAssunto = findViewById(R.id.etAssunto);
                 EditText etTexto = findViewById(R.id.etTexto);
 
-                // Guardando os dados em variaveis
+                // Registra os dados em variáveis.
                 String email = etEmail.getText().toString();
                 String assunto = etAssunto.getText().toString();
                 String texto = etTexto.getText().toString();
 
-                // Criando uma intent, como não exite mais uma tela ele irá para o link indicado no setData que é o do email
+                // Cria uma intent, como não existe mais uma tela, ele irá para o link indicado no setData que é o email.
                 Intent i = new Intent(Intent.ACTION_SENDTO);
                 i.setData(Uri.parse("mailto:"));
 
-                //Mandando os valores das variaveis para os campos do email, como: email, assunto e o texto
+                //Mandando os valores das variáveis para os campos do email, como: email, assunto e o texto.
                 String[] emails = new String[]{email};
                 i.putExtra(Intent.EXTRA_EMAIL, emails);
                 i.putExtra(Intent.EXTRA_SUBJECT, assunto);
                 i.putExtra(Intent.EXTRA_TEXT, texto);
 
-                // Caso o usuário tenha mais apps que aceitam o compartilhamento, caso tenha você escolhe o app, caso não, o android te mostra a mensagem de erro
+                // Caso o usuário tenha mais apps que aceitam o compartilhamento, caso tenha você escolhe o app, caso não, o android te mostra a mensagem de erro.
                 try {
                     startActivity(Intent.createChooser(i, "Escolha o APP"));
                 } catch (ActivityNotFoundException e) {
